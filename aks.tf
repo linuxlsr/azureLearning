cd71c0942fff3cd3479a8d25d8aada2192f42231
Charles Harvey <soops@ucla.edu>
2020-04-26 04:35:28 +0000

    stand up basic aks cluster, silence provider warning on init

---
+++ aks.tf
@@ -0,0 +1,27 @@
+module "service-principal" {
+  source   = "innovationnorway/service-principal/azurerm"
+  version  = "1.0.4"
+  years    = 1
+  end_date = "2020-05-31T00:00:00Z"
+  name     = "aks-service-principal"
+  password = var.db_password
+  role     = ""
+  scopes   = []
+}
+
+module "aks" {
+  source                         = "Azure/aks/azurerm"
+  version                        = "4.0.0"
+  resource_group_name            = azurerm_resource_group.first_rg.name
+  prefix                         = "linuslsr"
+  client_id                      = module.service-principal.client_id
+  client_secret                  = module.service-principal.client_secret
+  admin_username                 = var.admin_username
+  agents_size                    = "Standard_D2s_v3"
+  log_analytics_workspace_sku    = "PerGB2018"
+  log_retention_in_days          = 14
+  agents_count                   = 3
+  public_ssh_key                 = file("./id_rsa.pub")
+  tags                           = merge({ Name = "first resource group" }, local.common_tags)
+  enable_log_analytics_workspace = false
+}
--- provider.tf
+++ provider.tf
@@ -4,6 +4,21 @@ provider "azurerm" {
   version = ">2.5.0"
 }
 
+provider "random" {
+  version = "~>2.2"
+}
+
+provider "tls" {
+  version = "~>2.1"
+}
+
+provider "azuread" {
+  version = "~>0.8"
+}
+
+provider "local" {
+  version = "~>1.4"
+}
 # backend config vars removed and executed as a script that gets access key and initializes remote backend.
 terraform {
   backend "azurerm" {}
