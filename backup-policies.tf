ac9b45f3aeabe101501bfe77036edb548936f5c7
Charles Harvey <soops@ucla.edu>
2020-08-09 15:09:39 +0000

    remove rsv to limit cost, issues with soft-delete, backup policy that would not die

--- backup-policies.tf
+++ backup-policies.tf
@@ -1,19 +1,19 @@
-resource "azurerm_backup_policy_vm" "my-vm-bkp-policy" {
-  name                = "my-vm-backup-policy"
-  resource_group_name = azurerm_resource_group.first_rg.name
-  recovery_vault_name = azurerm_recovery_services_vault.gsa_vault.name
-  //timezone            = "UTC" # can't find other options in docs. default is UTC, so worthless to have
-
-  backup {
-    frequency = "Daily"
-    time      = "02:00"
-  }
-
-  retention_daily {
-    count = 30
-  }
-
-}
+//resource "azurerm_backup_policy_vm" "my-vm-bkp-policy" {
+//  name                = "my-vm-backup-policy"
+//  resource_group_name = azurerm_resource_group.first_rg.name
+//  recovery_vault_name = azurerm_recovery_services_vault.gsa_vault.name
+//  //timezone            = "UTC" # can't find other options in docs. default is UTC, so worthless to have
+//
+//  backup {
+//    frequency = "Daily"
+//    time      = "02:00"
+//  }
+//
+//  retention_daily {
+//    count = 30
+//  }
+//
+//}
 
 //resource "azurerm_backup_protected_vm" "first-vm-backup-protect" {
 //  backup_policy_id    = azurerm_backup_policy_vm.my-vm-bkp-policy.id
--- recovery-service-vaults.tf
+++ recovery-service-vaults.tf
@@ -1,11 +1,11 @@
 # recovery service vaults
-resource "azurerm_recovery_services_vault" "gsa_vault" {
-  location            = var.location
-  name                = "gsa-rsv"
-  resource_group_name = azurerm_resource_group.first_rg.name
-  sku                 = "Standard"
-  soft_delete_enabled = false
-}
+//resource "azurerm_recovery_services_vault" "gsa_vault" {
+//  location            = var.location
+//  name                = "gsa-rsv"
+//  resource_group_name = azurerm_resource_group.first_rg.name
+//  sku                 = "Standard"
+//  soft_delete_enabled = false
+//}
 
 resource "azurerm_recovery_services_vault" "tfstate_vault" {
   location            = var.location
